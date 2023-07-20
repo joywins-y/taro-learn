0ec1b92691718833b4617d19e79ae291a46ffbe2
windy <1519105541@qq.com>
2023-07-20 16:09:33 +0800

    feat: 用 useState 来重构 index 页面，将整个 Index 组件转换成函数式组件

---
+++ ultra-club/src/pages/index/index-old.tsx
@@ -0,0 +1,61 @@
+import { Component, PropsWithChildren } from 'react'
+import { View } from '@tarojs/components'
+import { PostCard, PostForm } from '../../components'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.scss'
+
+export default class Index extends Component<PropsWithChildren> {
+  state = {
+    posts: [
+      { title: '示例标题', content: '示例内容' }
+    ],
+    formTitle: '',
+    formContent: '',
+  }
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    const { formTitle: title, formContent: content } = this.state;
+    const newPosts = this.state.posts.concat({ title, content })
+
+    this.setState({
+      posts: newPosts,
+      formTitle: '',
+      formContent: ''
+    })
+  }
+
+  handleTitleInput(e) {
+    this.setState({ formTitle: e.target.value })
+  }
+
+  handleContentInput(e) {
+    this.setState({ formContent: e.target.value })
+  }
+
+  render() {
+    return (
+      <View className='index'>
+        {this.state.posts.map((post, index) => (
+          <PostCard key={index} title={post.title} content={post.content} />
+        ))}
+        <PostForm
+          formTitle={this.state.formTitle}
+          formContent={this.state.formContent}
+          handleSubmit={e => this.handleSubmit(e)}
+          handleTitleInput={e => this.handleTitleInput(e)}
+          handleContentInput={e => this.handleContentInput(e)}
+        />
+      </View>
+    )
+  }
+}
--- ultra-club/src/pages/index/index.tsx
+++ ultra-club/src/pages/index/index.tsx
@@ -1,61 +1,29 @@
-import { Component, PropsWithChildren } from 'react'
-import { View } from '@tarojs/components'
-import { PostCard, PostForm } from '../../components'
+import { View } from "@tarojs/components"
+import { useState } from "react"
+import { PostCard, PostForm } from "../../components"
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
-import './index.scss'
+export default function Index() {
+  const [posts, setPosts] = useState([{ title: '示例标题', content: '示例内容' }])
+  const [formTitle, setFormTitle] = useState('')
+  const [formContent, setFormContent] = useState('')
 
-export default class Index extends Component<PropsWithChildren> {
-  state = {
-    posts: [
-      { title: '示例标题', content: '示例内容' }
-    ],
-    formTitle: '',
-    formContent: '',
-  }
-  componentDidMount() { }
-
-  componentWillUnmount() { }
-
-  componentDidShow() { }
-
-  componentDidHide() { }
-
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault()
 
-    const { formTitle: title, formContent: content } = this.state;
-    const newPosts = this.state.posts.concat({ title, content })
-
-    this.setState({
-      posts: newPosts,
-      formTitle: '',
-      formContent: ''
-    })
+    const newPosts = posts.concat({ title: formTitle, content: formContent })
+    setPosts(newPosts)
+    setFormTitle('')
+    setFormContent('')
   }
 
-  handleTitleInput(e) {
-    this.setState({ formTitle: e.target.value })
-  }
-
-  handleContentInput(e) {
-    this.setState({ formContent: e.target.value })
-  }
-
-  render() {
-    return (
-      <View className='index'>
-        {this.state.posts.map((post, index) => (
-          <PostCard key={index} title={post.title} content={post.content} />
-        ))}
-        <PostForm
-          formTitle={this.state.formTitle}
-          formContent={this.state.formContent}
-          handleSubmit={e => this.handleSubmit(e)}
-          handleTitleInput={e => this.handleTitleInput(e)}
-          handleContentInput={e => this.handleContentInput(e)}
-        />
-      </View>
-    )
-  }
+  return <View className='index'>
+    {posts.map((post, index) => (<PostCard key={index} title={post.title} content={post.content} isList />))}
+    <PostForm
+      formTitle={formTitle}
+      formContent={formContent}
+      handleSubmit={handleSubmit}
+      handleTitleInput={(e) => setFormTitle(e.target.value)}
+      handleContentInput={(e) => setFormContent(e.target.value)}
+    />
+  </View>
 }
